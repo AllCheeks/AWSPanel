9bcd532477635b198cf1321dfae800f43abcf754
zengjia <zengjia2@foxmail.com>
2024-04-30 22:13:20 +0800

    test2

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
-mkdir tmp
-CGO_ENABLED=1 go build
-mv Aws-Panel tmp/
+mkdir -p build/web  # 创建Build文件夹以及其中的web子文件夹
+CGO_ENABLED=1 go build  # 构建Go项目
+mv Aws-Panel build/  # 将构建好的Aws-Panel移动到Build文件夹中
 cd ./web/
-npm install
-npm run build
-mv dist/ ../tmp/web
-cd ../tmp/
-zip -r Aws-Panel.zip *
-mv Aws-Panel.zip ../
-cd ../
-rm -rf tmp
+npm install  # 安装依赖
+npm run build  # 构建前端项目
+mv dist/* ../build/web/  # 将构建好的前端文件移动到Build/web中
+cd ..
+rm -rf tmp  # 清理tmp文件夹
