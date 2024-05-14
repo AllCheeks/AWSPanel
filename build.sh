8c50b8da9538f9af2317dcb0414b30b47094ad82
root <root@localhost.localdomain>
2024-05-14 17:26:23 +0800

    TEST

--- build.sh
+++ build.sh
@@ -1,12 +1,27 @@
-mkdir tmp
+#!/bin/sh
+
+# 设置临时目录路径
+TMP_DIR=$(mktemp -d)
+
+# 设置环境变量
+export HOME=/root
+export GOCACHE=$HOME/.cache/go-build
+export GOPATH=$HOME/go
+export GOMODCACHE=$GOPATH/pkg/mod
+
+mkdir -p $GOCACHE
+mkdir -p $GOPATH
+mkdir -p $GOMODCACHE
+
+# 构建项目
 CGO_ENABLED=1 go build
-mv Aws-Panel tmp/
+mv Aws-Panel $TMP_DIR/
 cd ./web/
 npm install
 npm run build
-mv dist/ ../tmp/web
-cd ../tmp/
-zip -r Aws-Panel.zip *
-mv Aws-Panel.zip ../
+mv dist/ $TMP_DIR/web
+cd $TMP_DIR/
+mv Aws-Panel ../
+mv web ../Aws-Panel
 cd ../
-rm -rf tmp
+rm -rf $TMP_DIR
